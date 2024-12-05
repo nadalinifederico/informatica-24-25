ba38729c61c7cf61ca19ced3def00efa31cd8e85
nadalinifederico <federico.nadalini@galileiostiglia.edu.it>
2024-12-05 10:04:26 +0000

    esquozeresto_5dic

--- informatica/es1.5dicembre.c
+++ informatica/es1.5dicembre.c
@@ -3,7 +3,7 @@ il metodo delle sottrazioni sucessive*/
 
 #include <stdio.h>
 int main(){
-    int n1, n2, quoz=0, resto;
+    int n1, n2, temp, quoz=0, resto;
 
     do{
         printf("inserici il primo valore: ");
@@ -12,6 +12,11 @@ int main(){
         scanf("%d", &n2);
     }while (n1<0 || n2<=0);
     resto=n1;
+    if(n1<n2){
+        temp=n1;
+        n1=n2;
+        n2=temp;
+    }
     while(resto>=n2){
         resto=resto-n2;
         quoz++;
