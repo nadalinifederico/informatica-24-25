ec18be63777408f57f33ab3e292ea569a7272310
nadalinifederico <federico.nadalini@galileiostiglia.edu.it>
2025-09-29 09:29:02 +0000

    esercizio(2)

--- informatica/es_25092025.c
+++ informatica/es_25092025.c
@@ -6,35 +6,84 @@
 5. creiamo un nuovo array contenente solo valori dispari*/
 #include <stdio.h>
 #include <stdlib.h>
-#define N 5
 
-int main(){
-    int *array=NULL, n, somma=0;
-    printf("inserisci il numero di elementi dell'array: ");
-    scanf("%d", &n);
-    
-    array=(int*)malloc(n*sizeof(int));
-
-    if(array==NULL){
-        printf("errore allocazione memoria");
-        return 1;
+int* alloca_array(int n) {
+    int* arr=(int*)malloc(n * sizeof(int));
+    if (arr==NULL) {
+        printf("Errore allocazione memoria\n");
+        return NULL;
     }
-    printf("inserisci i valori dell'array:\n");
-    for(int i=0; i<n; i++){
+    return arr;
+}
+
+void inserisci_valori(int* arr, int n) {
+    printf("Inserisci i valori dell'array:\n");
+    for (int i=0; i<n; i++) {
         printf("elemento: ");
-        scanf("%d", &array[i]);
-        
-        if(array[i]%3==0){
-            somma+=array[i];
+        scanf("%d", &arr[i]);
+    }
+}
+
+void stampa_array(int* arr, int n) {
+    printf("L'array inserito e':\n");
+    for (int i=0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int somma_multipli_3(int* arr, int n) {
+    int somma=0;
+    for (int i=0; i<n; i++) {
+        if (arr[i]%3==0) {
+            somma+=arr[i];
         }
     }
-    printf("l'array inserito e':\n");
-    for(int i=0; i<n; i++){     
-        printf("%d ", array[i]);
-    }
-    printf("\nla somma degli elementi multipli di 3 e': %d\n", somma);
-    
-    free(array);
-    return 0;
+    return somma;
+}
 
+int conta_dispari(int* arr, int n) {
+    int conta=0;
+    for (int i=0; i<n; i++) {
+        if (arr[i]%2!=0) conta++;
+    }
+    return conta;
+}
+
+int* crea_array_dispari(int* arr, int n, int* n_dispari) {
+    *n_dispari = conta_dispari(arr, n);
+    int* dispari = (int*)malloc((*n_dispari) * sizeof(int));
+    if (dispari==NULL && *n_dispari>0) {
+        printf("Errore allocazione memoria nuovo array\n");
+        return NULL;
+    }
+    int j=0;
+    for (int i=0; i<n; i++) {
+        if (arr[i] %2!=0) {
+            dispari[j++]=arr[i];
+        }
+    }
+    return dispari;
+}
+
+int main() {
+    int *array=NULL, n;
+    printf("Inserisci il numero di elementi dell'array: ");
+    scanf("%d", &n);
+
+    array=alloca_array(n);
+    inserisci_valori(array, n);
+    stampa_array(array, n);
+
+    int somma=somma_multipli_3(array, n);
+    printf("La somma degli elementi multipli di 3 e': %d\n", somma);
+
+    int n_dispari;
+    int* array_dispari=crea_array_dispari(array, n, &n_dispari);
+    printf("Nuovo array con soli valori dispari:\n");
+    stampa_array(array_dispari, n_dispari);
+
+    free(array);
+    free(array_dispari);
+    return 0;
 }
